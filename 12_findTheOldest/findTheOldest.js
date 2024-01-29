d06cf308f1d13e41144722dcfef9bea206aff993
Yvette Ang <angyvette03@gmail.com>
2024-01-29 14:31:37 +0800

    Finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let person = array.reduce((oldest, currentPerson) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
+    return person;
 };
+
+const getAge = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
